b49e1ae513c7065d9ab9f7e59c505aed3a802ebc
makkoli <makkoli@vt.edu>
2017-03-22 15:42:08 -0400

    added user model

---
+++ models/user-model.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    findOrCreate = require('mongoose-findorcreate');
+
+var UserSchema = new Schema({
+    twitterId: {
+        type: String,
+        required: true,
+        index: { unique: true }
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    linked_images: {
+        type: [],
+        default: []
+    }},
+    {
+        collection: 'users'
+    }
+);
+
+UserSchema.plugin(findOrCreate);
+
+module.exports = mongoose.model('User', UserSchema);
